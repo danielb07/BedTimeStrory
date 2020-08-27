2d29d3a181f5ee34723c8c3b39d504645df6e9f9
danielb07 <danielbarraud07@gmail.com>
2020-08-27 15:53:12 +0530

    updated WriteScreen

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-
-import ReadStoryScreen from './screens/ReadStoryScreen';
-import WriteStoryScreen from './screens/WriteStoryScreen';
+import ReadScreen from './screens/ReadScreen';
+import WriteScreen from './screens/WriteScreen';
 export default function App() {
   return (
     <View style={styles.container}>
@@ -18,13 +17,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    
     padding: 8,
   },
 });
 
 const TabNavigation = createBottomTabNavigator({
-  ReadStoryScreen: {screen:ReadStoryScreen},
-  WriteStoryScreen: {screen:WriteStoryScreen}
+  ReadScreen: {screen:ReadScreen},
+  WriteScreen: {screen:WriteScreen}
+},
+
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "WriteScreen"){
+        return(
+          <Image
+          source={require("./assets/write.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "ReadScreen"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
 })
 
-const TabContainer= createAppContainer(TabNavigation)
+const TabContainer = createAppContainer(TabNavigation);
--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,33 +1,90 @@
 import React from 'react';
 import {
   View,
-  StyleSheet,
   Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput
 } from 'react-native';
-import { Header } from 'react-native-elements';
 
+import { Header } from 'react-native-elements';
 
 export default class WriteStoryScreen extends React.Component{
   render(){
     return(
       <View style = {styles.container}>
-        <Text style = {styles.text}>You can write your own story</Text>
+        <Header
+          backgroundColor={'#FFC0CB'}
+          centerComponent={{
+            text: 'Write Your Own Story',
+            style: { color: '#000000', fontSize: 18 },
+          }}
+        />
+
+        <TextInput
+        style={styles.inputBox}
+        placeholder="Title"
+        ></TextInput>
+
+        <TextInput
+        style={styles.inputBox}
+        placeholder="Author"
+        ></TextInput>
+        
+
+        <TextInput
+        style={styles.multipleInputBox}
+        placeholder="Your Story"
+        multiline
+        numberOfLines={4}
+        ></TextInput>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>SUBMIT</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  inputBox: {
+    width: '95%',
+    height: 40,
+    borderWidth: 1.5,
+    fontSize: 20,
+    marginLeft:10,
+    marginTop:20
+  },
+  multipleInputBox: {
+    width: '95%',
+    height: 250,
+    borderWidth: 1.5,
+    fontSize: 20,
+    marginLeft:10,
+    marginTop:20
+  },
   container: {
-    justifyContent:'center',
-    alignItems: 'center',
-    marginTop:250
+    flex: 1,
   },
 
-  text: {
+  button:{
+    marginTop:20,
+    marginLeft:100,
     justifyContent:'center',
     alignItems:'center',
-    fontSize:20
+    width:100,
+    height:50,
+    borderWidth:3,
+    borderRadius:'20px',
+    backgroundColor:'#FFC0CB'
+  },
+
+  buttonText:{
+    justifyContent:'center',
+    alignItems:'center',
+    fontStyle:'bold',
+    fontSize:18,
   }
 
 })
